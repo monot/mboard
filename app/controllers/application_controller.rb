681b1df96cba0e6c273f4f34ffbd589326a95c7e
Joonhwan Lee <joonhwan@gmail.com>
2017-06-04 19:19:54 +0900

    Basic CSS layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :old_affiliation, :affiliation, :email, :password) }
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :old_affiliation, :affiliation, :email, :password, :current_password) }
     end
+    
 end
