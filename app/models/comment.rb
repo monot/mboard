e7d30fc4fe4f007c1dcd0fff683d3e5ad6d377e5
Joonhwan Lee <joonhwan@gmail.com>
2017-06-11 10:02:20 +0900

    Model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :message
   belongs_to :user
+
+  validates_presence_of :content, :message => "답글을 입력해주세요."
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates_presence_of :content, :message => "메시지를 입력해주세요."
 end
